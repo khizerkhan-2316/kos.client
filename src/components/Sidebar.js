8973283bf9d8d09f20c40060993de08f04be9b1c
Zerefez <johnnguyen28901@gmail.com>
2024-04-17 19:00:44 +0200

    Added graph

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,68 +1,69 @@
-import React from 'react'
+import React from 'react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { MdHome } from "react-icons/md";
 import { RiDashboard2Fill } from "react-icons/ri";
 import { AiOutlineControl } from "react-icons/ai";
-import { FaStickyNote,FaChevronRight,} from "react-icons/fa"
+import { FaStickyNote, FaChevronRight, FaRegCopyright } from "react-icons/fa";
 
 function Sidebar() {
 
-  const [highlight, setHighlight] = useState(
-    {
-      Homepage: true,
-      dashboard: false,
-      control: false,
-      logs: false,
+  const [highlight, setHighlight] = useState({
+    Homepage: true,
+    dashboard: false,
+    control: false,
+    logs: false,
+  });
 
+  function toggleHighlight(page) {
+    const newHighlight = { ...highlight };
+    Object.keys(newHighlight).forEach((key) => {
+      newHighlight[key] = false;
     });
+    newHighlight[page] = true;
+    setHighlight(newHighlight);
+  }
 
-    function toggleHighlight(page) {
-
-      const newHighlight = { ...highlight };
-      Object.keys(newHighlight).forEach((key) => {
-        newHighlight[key] = false;
-      });
-      newHighlight[page] = true;
-      setHighlight(newHighlight);
-    }
-
-
-    return (
-        <div className='bg-[#1c1c1c] px-[25px] w-[13%] h-screen fixed'>
-            <div className='px-[15px] py-[30px] flex items-center justify-center border-b-[1px] border-[#ededed]/[0.3]'>
-                <h1 className='text-white text-[20px] leading-[24px] font-extrabold cursor-pointer'>Admin panel</h1>
-            </div>
-            <div className='rounded flex items-center gap-[15px] py-[10px] border-b-[1px] border-[#EDEDED]/[0.3] cursor-pointer' style={highlight.Homepage? {backgroundColor: "#1976D2"} : {}}>
-                <MdHome color='white' />
-                <Link onClick = {() => toggleHighlight("Homepage")} to="/" className='text-[14px] leading-[20px] font-bold text-white'>Homepage</Link>
-            </div>
-            <div className='pt-[15px] border-b-[1px] border-[#EDEDED]/[0.3]'>
-                <p className='text-[10px] font-extrabold leading-[16px] text-white/[0.4]'> INTERFACE</p>
-                <div className='flex items-center justify-between gap-[10px] py-[10px] cursor-pointer rounded' style={highlight.dashboard? {backgroundColor: "#1976D2"} : {}}>
-                    <div className='flex items-center gap-[10px]'>
-                        <RiDashboard2Fill color='white' /> <Link onClick = {() => toggleHighlight("dashboard")} to="/dashboard" className='text-[14px] leading-[20px] font-normal text-white'>Dashboard</Link>
-                    </div>
-                    <FaChevronRight color='white' />
-                </div>
-                <div className='flex items-center justify-between gap-[10px] py-[10px] rounded cursor-pointer' style={highlight.control? {backgroundColor: "#1976D2"} : {}}>
-                    <div className='flex items-center gap-[10px]'>
-                        <AiOutlineControl color='white' /> <Link onClick = {() => toggleHighlight("control")} to="/control" className='text-[14px] leading-[20px] font-normal text-white'>Actuator control</Link>
-                    </div>
-                    <FaChevronRight color='white' />
-                </div>
-                </div>
-            <div className='pt-[15px] border-b-[1px] border-[#EDEDED]/[0.3]'>
-                <p className='text-[10px] font-extrabold leading-[16px] text-white/[0.4]'> ADDONS</p>
-                <div className='flex items-center justify-between gap-[10px] py-[10px] rounded cursor-pointer' style={highlight.logs? {backgroundColor: "#1976D2"} : {}}>
-                    <div className='flex items-center gap-[10px]'>
-                        <FaStickyNote color='white' /><Link onClick = {() => toggleHighlight("logs")} to ="/logs" className='text-[14px] leading-[20px] font-normal text-white'>Logs</Link>
-                    </div>
-                    <FaChevronRight color='white' />
-                </div>
-            </div>
+  return (
+    <div className='bg-[#1c1c1c] px-[25px] w-[13%] h-screen fixed'>
+      <div className='px-[15px] py-[30px] flex items-center justify-center border-b-[1px] border-[#ededed]/[0.3]'>
+        <h1 className='text-white text-[20px] leading-[24px] font-extrabold cursor-pointer'>Admin panel</h1>
+      </div>
+      <div className='rounded flex items-center gap-[15px] py-[10px] border-b-[1px] border-[#EDEDED]/[0.3] cursor-pointer' style={highlight.Homepage ? { backgroundColor: "#1976D2" } : {}}>
+        <MdHome color='white' />
+        <Link onClick={() => toggleHighlight("Homepage")} to="/" className='text-[14px] leading-[20px] font-bold text-white'>Homepage</Link>
+      </div>
+      <div className='pt-[15px] border-b-[1px] border-[#EDEDED]/[0.3]'>
+        <p className='text-[10px] font-extrabold leading-[16px] text-white/[0.4]'> INTERFACE</p>
+        <div className='flex items-center justify-between gap-[10px] py-[10px] cursor-pointer rounded' style={highlight.dashboard ? { backgroundColor: "#1976D2" } : {}}>
+          <div className='flex items-center gap-[10px]'>
+            <RiDashboard2Fill color='white' /> <Link onClick={() => toggleHighlight("dashboard")} to="/dashboard" className='text-[14px] leading-[20px] font-normal text-white'>Dashboard</Link>
+          </div>
+          <FaChevronRight color='white' />
         </div>
-    )
+        <div className='flex items-center justify-between gap-[10px] py-[10px] rounded cursor-pointer' style={highlight.control ? { backgroundColor: "#1976D2" } : {}}>
+          <div className='flex items-center gap-[10px]'>
+            <AiOutlineControl color='white' /> <Link onClick={() => toggleHighlight("control")} to="/control" className='text-[14px] leading-[20px] font-normal text-white'>Actuator control</Link>
+          </div>
+          <FaChevronRight color='white' />
+        </div>
+      </div>
+      <div className='pt-[15px] border-b-[1px] border-[#EDEDED]/[0.3]'>
+        <p className='text-[10px] font-extrabold leading-[16px] text-white/[0.4]'> ADDONS</p>
+        <div className='flex items-center justify-between gap-[10px] py-[10px] rounded cursor-pointer' style={highlight.logs ? { backgroundColor: "#1976D2" } : {}}>
+          <div className='flex items-center gap-[10px]'>
+            <FaStickyNote color='white' /><Link onClick={() => toggleHighlight("logs")} to="/logs" className='text-[14px] leading-[20px] font-normal text-white'>Logs</Link>
+          </div>
+          <FaChevronRight color='white' />
+        </div>
+      </div>
+      <div className='absolute bottom-[20px] left-[55px]'>
+        <div className='flex items-center gap-[10px]'>
+          <FaRegCopyright color='white' /><p className='text-[14px] leading-[20px] font-normal text-white'>By Group 8</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,76 @@
+import React from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+const data = [
+  {
+    time: '8:00 AM',
+    temperature: 22,
+    co2Level: 400,
+  },
+  {
+    time: '9:00 AM',
+    temperature: 22.5,
+    co2Level: 410,
+  },
+  {
+    time: '10:00 AM',
+    temperature: 15,
+    co2Level: 300,
+  },
+  {
+    time: '11:00 AM',
+    temperature: 23.5,
+    co2Level: 430,
+  },
+  {
+    time: '12:00 PM',
+    temperature: 24,
+    co2Level: 435,
+  },
+  {
+    time: '1:00 PM',
+    temperature: 10,
+    co2Level: 200,
+  },
+  {
+    time: '2:00 PM',
+    temperature: 23,
+    co2Level: 400,
+  },
+];
+
 function Dashboard() {
   return (
     <div className="w-[87%] border ml-[13%]">
-      <h1>Dashboard</h1>
+      <h1 className="mt-[100px] mb-4 text-4xl font-semibold leading-tight text-gray-900 md:text-5xl lg:text-6xl">Dashboard</h1>
+      <h2 className="mt-[50px] mb-4 text-3xl font-semibold text-gray-900">Temperature and CO2 Level</h2>
+     
+      <div className='flex'>
+        <div className='basic-[70%]'>
+        <LineChart
+          width={1000}
+          height={600}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="time" />
+          <YAxis yAxisId="left" label={{ value: 'Temperature (°C)', angle: -90, position: 'insideLeft' }} />
+          <YAxis yAxisId="right" label={{ value: 'CO2 Level (ppm)', angle: -90, position: 'insideRight' }} orientation="right" />
+          <Tooltip />
+          <Legend />
+          <Line yAxisId="left" type="monotone" dataKey="temperature" stroke="#8884d8" activeDot={{ r: 8 }} name="Temperature (°C)" />
+          <Line yAxisId="right" type="monotone" dataKey="co2Level" stroke="#82ca9d" name="CO2 Level (ppm)" />
+        </LineChart>
+        </div>
+        <div className='basic-[30%]'>
+      </div>
+    </div>
     </div>
   );
 }
