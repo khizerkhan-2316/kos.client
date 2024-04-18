3bcbb6fd43385c908fb4354a06965a110ffa4a50
Khizer Khan <khizer759@gmail.com>
2024-04-18 10:23:23 +0200

    made navlinks more accessable

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { MdHome } from "react-icons/md";
-import { RiDashboard2Fill } from "react-icons/ri";
-import { AiOutlineControl } from "react-icons/ai";
-import { FaStickyNote, FaChevronRight, FaRegCopyright } from "react-icons/fa";
+import { MdHome } from 'react-icons/md';
+import { RiDashboard2Fill } from 'react-icons/ri';
+import { AiOutlineControl } from 'react-icons/ai';
+import { FaStickyNote, FaChevronRight, FaRegCopyright } from 'react-icons/fa';
 
 function Sidebar() {
-
   const [highlight, setHighlight] = useState({
     Homepage: true,
     dashboard: false,
@@ -25,41 +24,92 @@ function Sidebar() {
   }
 
   return (
-    <div className='bg-[#1c1c1c] px-[25px] w-[13%] h-screen fixed'>
-      <div className='px-[15px] py-[30px] flex items-center justify-center border-b-[1px] border-[#ededed]/[0.3]'>
-        <h1 className='text-white text-[20px] leading-[24px] font-extrabold cursor-pointer'>Admin panel</h1>
+    <div className="bg-[#1c1c1c] px-[25px] w-[60%] sm:w-[40%] md:w-[30%] lg:w-[25%] xl:w-[20%] 2xl:w-[13%] h-screen fixed">
+      <div className="px-[15px] py-[30px] flex items-center justify-center border-b-[1px] border-[#ededed]/[0.3]">
+        <h1 className="text-white text-[20px] leading-[24px] font-extrabold cursor-pointer">
+          Admin panel
+        </h1>
       </div>
-      <div className='rounded flex items-center gap-[15px] py-[10px] border-b-[1px] border-[#EDEDED]/[0.3] cursor-pointer' style={highlight.Homepage ? { backgroundColor: "#1976D2" } : {}}>
-        <MdHome color='white' />
-        <Link onClick={() => toggleHighlight("Homepage")} to="/" className='text-[14px] leading-[20px] font-bold text-white'>Homepage</Link>
-      </div>
-      <div className='pt-[15px] border-b-[1px] border-[#EDEDED]/[0.3]'>
-        <p className='text-[10px] font-extrabold leading-[16px] text-white/[0.4]'> INTERFACE</p>
-        <div className='flex items-center justify-between gap-[10px] py-[10px] cursor-pointer rounded' style={highlight.dashboard ? { backgroundColor: "#1976D2" } : {}}>
-          <div className='flex items-center gap-[10px]'>
-            <RiDashboard2Fill color='white' /> <Link onClick={() => toggleHighlight("dashboard")} to="/dashboard" className='text-[14px] leading-[20px] font-normal text-white'>Dashboard</Link>
-          </div>
-          <FaChevronRight color='white' />
+      <Link
+        onClick={() => toggleHighlight('Homepage')}
+        to="/"
+        className="text-[14px] leading-[20px] font-bold text-white"
+      >
+        <div
+          className="rounded flex items-center gap-[15px] pl-3 py-[10px] border-b-[1px] border-[#EDEDED]/[0.3] cursor-pointer"
+          style={highlight.Homepage ? { backgroundColor: '#1976D2' } : {}}
+        >
+          <MdHome color="white" />
+          Homepage
         </div>
-        <div className='flex items-center justify-between gap-[10px] py-[10px] rounded cursor-pointer' style={highlight.control ? { backgroundColor: "#1976D2" } : {}}>
-          <div className='flex items-center gap-[10px]'>
-            <AiOutlineControl color='white' /> <Link onClick={() => toggleHighlight("control")} to="/control" className='text-[14px] leading-[20px] font-normal text-white'>Actuator control</Link>
+      </Link>
+
+      <div className="pt-[15px]  border-b-[1px] border-[#EDEDED]/[0.3]">
+        <p className="text-[10px]  font-extrabold leading-[16px] pl-3 text-white/[0.4]">
+          {' '}
+          INTERFACE
+        </p>
+        <Link
+          onClick={() => toggleHighlight('dashboard')}
+          to="/dashboard"
+          className="text-[14px] leading-[20px] font-normal text-white"
+        >
+          <div
+            className="flex items-center mt-1 justify-between gap-[10px] py-[10px] cursor-pointer rounded"
+            style={highlight.dashboard ? { backgroundColor: '#1976D2' } : {}}
+          >
+            <div className="flex pl-3 items-center gap-[10px]">
+              <RiDashboard2Fill color="white" /> Dashboard
+            </div>
+
+            <FaChevronRight color="white" />
           </div>
-          <FaChevronRight color='white' />
-        </div>
-      </div>
-      <div className='pt-[15px] border-b-[1px] border-[#EDEDED]/[0.3]'>
-        <p className='text-[10px] font-extrabold leading-[16px] text-white/[0.4]'> ADDONS</p>
-        <div className='flex items-center justify-between gap-[10px] py-[10px] rounded cursor-pointer' style={highlight.logs ? { backgroundColor: "#1976D2" } : {}}>
-          <div className='flex items-center gap-[10px]'>
-            <FaStickyNote color='white' /><Link onClick={() => toggleHighlight("logs")} to="/logs" className='text-[14px] leading-[20px] font-normal text-white'>Logs</Link>
+        </Link>
+
+        <Link
+          onClick={() => toggleHighlight('control')}
+          to="/control"
+          className="text-[14px] leading-[20px] font-normal text-white"
+        >
+          <div
+            className="flex items-center justify-between gap-[10px] py-[10px] rounded cursor-pointer"
+            style={highlight.control ? { backgroundColor: '#1976D2' } : {}}
+          >
+            <div className="flex pl-3 items-center gap-[10px]">
+              <AiOutlineControl color="white" /> Actuator control
+            </div>
+            <FaChevronRight color="white" />
           </div>
-          <FaChevronRight color='white' />
-        </div>
+        </Link>
       </div>
-      <div className='absolute bottom-[20px] left-[55px]'>
-        <div className='flex items-center gap-[10px]'>
-          <FaRegCopyright color='white' /><p className='text-[14px] leading-[20px] font-normal text-white'>By Group 8</p>
+      <div className="pt-[15px] border-b-[1px] border-[#EDEDED]/[0.3]">
+        <p className="text-[10px] font-extrabold leading-[16px] pl-3 text-white/[0.4]">
+          {' '}
+          ADDONS
+        </p>
+        <Link
+          onClick={() => toggleHighlight('logs')}
+          to="/logs"
+          className="text-[14px] leading-[20px] font-normal text-white"
+        >
+          <div
+            className="flex items-center justify-between mt-1 gap-[10px] py-[10px] rounded cursor-pointer"
+            style={highlight.logs ? { backgroundColor: '#1976D2' } : {}}
+          >
+            <div className="flex pl-3 items-center gap-[10px]">
+              <FaStickyNote color="white" />
+              Logs
+            </div>
+            <FaChevronRight color="white" />
+          </div>
+        </Link>
+      </div>
+      <div className="absolute bottom-[20px] left-[55px]">
+        <div className="flex items-center gap-[10px]">
+          <FaRegCopyright color="white" />
+          <p className="text-[14px] leading-[20px] font-normal text-white">
+            By Group 8
+          </p>
         </div>
       </div>
     </div>
